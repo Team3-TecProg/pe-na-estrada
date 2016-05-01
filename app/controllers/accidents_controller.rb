01d98406b1afe0abdf1180065457aa7afa53aeb1
arthurTemporim <arthurrtl@gmail.com>
2016-05-01 01:11:18 -0300

    Apply Header in accidents_controler.

--- app/controllers/accidents_controller.rb
+++ app/controllers/accidents_controller.rb
@@ -1,2 +1,7 @@
+######################################################################
+# Class name: AccidentsController
+# File name: accidents_controller.rb
+# Description: This controller is empty. 
+######################################################################
 class AccidentsController < ApplicationController
 end
