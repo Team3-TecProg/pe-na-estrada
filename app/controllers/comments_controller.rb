abd69620b3f0117fe9ddaa0db204d3f9fc662ac3
Matheus Miranda <matheusmirandalacerda@gmail.com>
2016-05-03 19:20:33 -0300

    Fix size of lines and add assertions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,29 @@
 ######################################################################
 # Class name: CommentsController
 # File name: comments_controller.rb
-# Description: Controller used to communicate with the view highways/show
+# Description: Controller used to communicate with the view..
+# ..highways/show
 ######################################################################
 
 class CommentsController < ApplicationController
+
+    #This method returns a variable called '@route' that contains..
+    #..a new object of the route.
+    # This method is called from the view package to show the web..
+    #..page to the user too.
     def index
         @comment = Comment.new
     end
 
-    #Generates an object of the Highway model and saves it in the @highway variable
+    #Generates an object of the Highway model and saves it in the..
+    #..@highway variable
+
     def create
         # Get the highway that will receive the comment
         @highway = Highway.find( params[ :highway_id ] )
+        assert_object_is_not_null( @highway )
         @comment = Comment.new( comment_params )
+        assert_object_is_not_null( @comment )
         if ( @comment.save )
             @comment.save
             redirect_to highway_path( @highway )
