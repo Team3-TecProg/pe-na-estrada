d3236980ca5c2392940606e7dc2f339d146a9b1c
arthurTemporim <arthurrtl@gmail.com>
2016-05-06 00:31:45 -0300

    Add description of methods in accidents class

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -9,23 +9,40 @@ class Accident < ActiveRecord::Base
 
     belongs_to :Highway
 
+    # Description: Counts the number of accidents in one highway.
+    # Parameters: none.
+    # Return : group().
     def self.count_accidents
+        # Number of the chosen highways.
         group(:br).count
     end
 
+    # Description: The total of accidents thats happends in one highway.
+    # Parameters: none.
+    # Return : total_accidents.
     def self.total_accidents
+        # Recives the number of total accidents in one especific highway.
  	      total_accidents = Accident.count
         return total_accidents
     end
 
+    # Description: Take the accidents from the accident latitude perspective.
+    # Parameters: none.
+    # Return : all.map.
     def self.get_accidents_latitude
         all.map &:latitude
     end
 
+    # Description: Take the accidents from the accident longitude perspective.
+    # Parameters: none.
+    # Return : all.map.
     def self.get_accidents_longitude
         all.map &:longitude
     end
 
+    # Description: Take the highway number.
+    # Parameters: none.
+    # Return : all.map.
     def self.get_accidents_highway_number
         all.map &:highway_number
     end
