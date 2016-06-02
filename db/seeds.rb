24a40a4dcd90ef0cdcd7fdb7441559f567afdbbc
ArthurTemporim <arthurrtl@gmail.com>
2016-06-01 23:08:48 -0300

    Routes seeds made

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# This file contain all the record creation needed to seed the database with
+# its default values.
+
+# Route.
+# Create 5 generic routes.
+5.times do |i|
+  Route.create(origin: "Origin ##{i}", destination: "Destination ##{i}")
+end
